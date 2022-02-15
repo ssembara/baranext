d8efbc4270568320d1db312c189838a3cc79f28b
Sebastianus Sembara <sembara9090@gmail.com>
2022-02-15 10:41:23 +0700

    init: clean dir, add chakra-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { Box, Container } from '@chakra-ui/react'
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
